4ef38b5b84af69e3efbe3ce643acc70f7ab9f3cf
Alex Alexandrov <alexandrov.alex.al@gmail.com>
2023-02-07 09:27:06 +0200

    Remove redundant code.

--- app/src/main/java/com/example/testapplication/config/AppConfig.kt
+++ app/src/main/java/com/example/testapplication/config/AppConfig.kt
@@ -3,7 +3,6 @@ package com.example.testapplication.config
 import android.app.Application
 import com.example.testapplication.dependency.unit.mainDatabase
 import com.example.testapplication.dependency.unit.inventoryConnector
-import com.example.testapplication.dependency.unit.itemService
 import com.example.testapplication.dependency.unit.orderConnector
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
@@ -13,7 +12,7 @@ class AppConfig : Application() {
         super.onCreate()
         startKoin {
             androidContext(this@AppConfig)
-            modules(listOf(inventoryConnector, orderConnector, mainDatabase, itemService))
+            modules(listOf(inventoryConnector, orderConnector, mainDatabase))
         }
     }
 }
--- app/src/main/java/com/example/testapplication/database/MainDatabase.kt
+++ app/src/main/java/com/example/testapplication/database/MainDatabase.kt
@@ -1,39 +1,11 @@
 package com.example.testapplication.database
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.testapplication.database.dao.ItemDao
 import com.example.testapplication.database.entity.Item
-import org.koin.core.component.KoinComponent
 
 @Database(entities = [Item::class], version = 3, exportSchema = false)
-abstract class MainDatabase : RoomDatabase(), KoinComponent {
+abstract class MainDatabase : RoomDatabase() {
     abstract fun itemDao(): ItemDao
-
-    companion object {
-        private var INSTANCE: MainDatabase? = null
-
-        fun getDatabase(context: Context): MainDatabase {
-            val tempInstance = INSTANCE
-            if (tempInstance != null) {
-                return tempInstance
-            }
-            return if (INSTANCE != null) {
-                INSTANCE as MainDatabase
-            } else {
-                val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    MainDatabase::class.java,
-                    "main_database"
-                )
-                    .allowMainThreadQueries()
-                    .fallbackToDestructiveMigration()
-                    .build()
-                INSTANCE = instance
-                instance
-            }
-        }
-    }
 }
